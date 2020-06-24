d6536a26839eab52ce074d77acecce9035932b69
juanobrach <juanobrach@gmail.com>
2020-06-23 21:29:48 -0300

    apply changes into main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { SelectionComponent } from './selection/selection.component';
+import { DrinkSizeComponent } from './drink-size/drink-size.component';
+import { CoreModule } from './core/core.module';
+import { PersonalizationComponent } from './personalization/personalization.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SelectionComponent,
+    DrinkSizeComponent,
+    PersonalizationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
