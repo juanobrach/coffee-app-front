a7377488026ec314096079812ba9cee66761bbc4
juanobrach <juanobrach@gmail.com>
2020-06-24 01:09:17 -0300

    preselect regular size of drinkt type as initial render and create a hihglight condition for the selector

--- src/app/core/store/order.reducer.ts
+++ src/app/core/store/order.reducer.ts
@@ -3,17 +3,24 @@ import { createReducer, on, createSelector } from '@ngrx/store';
 import { addProduct, setDrinkSize } from './order.actions';
 import { Product } from '../models/Product';
 import { Order } from '../models/Order';
+import { state } from '@angular/animations';
 
 export const initialState: Order = {
   product: {} as Product,
-  size: '',
+  size: 'regular',
   extras: [],
   total: 0,
 };
 
+// addProduct has as default the regular price since the use could choice the drink size after the selecttion of the drink type.
 const _orderReducer = createReducer(initialState,
-  on( addProduct, (state, {product}) => ({ ...state, product: { ...product}})),
-  on( setDrinkSize, (state, {size} ) => ({ ...state,  size }))
+  on( addProduct, (state, {product}) => ({ ...state, product: { ...product}, total: product.price })),
+  on( setDrinkSize, (state, {size} ) => {
+    // Change the total of the order based on the selected size of the drink
+    const productPrice = state.product.price;
+    const total = ( size === 'regular' ? productPrice : productPrice * 2 );
+    return({ ...state,  size, total })
+  })
 );
 
 export function orderReducer(state, action) {
--- src/app/drink-size/drink-size.component.ts
+++ src/app/drink-size/drink-size.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { setDrinkSize  } from '../core/store';
 import { Order } from './../core/models/Order';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-drink-size',
@@ -9,13 +10,22 @@ import { Order } from './../core/models/Order';
   styleUrls: ['./drink-size.component.scss']
 })
 export class DrinkSizeComponent implements OnInit {
+  order$: Observable<Order>;
   @Input() regularPrice;
   largePrice;
+  drinkSize = 'regular';
 
-  constructor(private store: Store<{ order: Order }>) {}
 
+
+  constructor(private store: Store<{ order: Order }>) {
+    this.order$ = store.select( state => state.order );
+  }
+  
   ngOnInit(): void {
     this.largePrice = this.regularPrice * 2;
+    this.order$.subscribe( order => {
+      this.drinkSize = order.size;
+    })
   }
 
   selectDrinkSize(size){
